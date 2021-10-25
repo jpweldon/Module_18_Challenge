ecb28cdacd4fd6c83a87ad6f45ca2d9314d20ce9
John Weldon <85202691+jpweldon@users.noreply.github.com>
2021-10-25 00:31:26 -0400

    Create a Record Data Class that consists of the sender, receiver, and  amount attributes

--- pychain.py
+++ pychain.py
@@ -49,8 +49,13 @@ import hashlib
 # @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
 
+@dataclass
+class Record:
+
+    sender: str
+    receiver: str
+    amount: float
 
 ################################################################################
 # Step 2:
